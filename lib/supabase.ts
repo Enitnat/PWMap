dfe94bcc974ef170489b0e589de8ff19b4fa5816
Cons Feje <fejecons@gmail.com>
2026-02-28 13:20:30 +0800

    bug fix

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,7 +3,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL as string;
-const supabasePubKey = process.env.EXPO_PUBLIC_SUPABASE_PUBLISHABLE_KEY as string;
+const supabasePubKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY as string;
 
 export const supabase = createClient(supabaseUrl, supabasePubKey, {
   auth: {
